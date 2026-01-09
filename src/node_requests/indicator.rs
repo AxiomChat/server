eee631ee6f78a108adda9ee85dd2e8b5c7f94272
Leo dev <klesti.selimay@icloud.com>
2026-01-09 17:58:13 +0100

    Node mode

---
+++ src/node_requests/indicator.rs
@@ -0,0 +1,6 @@
+use crate::{server::Server, utils::client::Client};
+use std::sync::Arc;
+
+pub fn start_typing(server: &Arc<Server>, client: &Client, channel_id: &str) -> crate::Result<()> {
+    Ok(())
+}
